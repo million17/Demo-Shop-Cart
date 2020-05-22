d98042f47b8204384e5a1f688ed917ef211d5c60
trieu.ngoquang <trieu.ngoquang@vti.com.vn>
2020-05-22 15:18:23 +0700

    update final upload image profile

--- src/main/java/application/controller/api/UploadApiController.java
+++ src/main/java/application/controller/api/UploadApiController.java
@@ -1,9 +1,13 @@
 package application.controller.api;
 
+import application.model.api.FileUploadResult;
 import application.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/upload")
@@ -11,5 +15,30 @@ public class UploadApiController {
 
     @Autowired
     private FileStorageService storageService;
+
+    @PostMapping("/upload-image")
+    public FileUploadResult uploadResult(
+            @RequestParam("file") MultipartFile multipartFile
+    ) {
+        String message = "";
+        FileUploadResult result = new FileUploadResult();
+
+
+        try {
+            String newFileName = storageService.store(multipartFile);
+            message = "You successfully upload " + multipartFile.getOriginalFilename() + "!";
+
+            result.setSuccess(true);
+            result.setMessage(message);
+            result.setLink("/link/" + newFileName);
+
+        } catch (Exception ex) {
+            result.setMessage(ex.getMessage());
+            result.setSuccess(false);
+        }
+
+        return result;
+
+    }
 
 }
