d24a1a646e00bcf637927968a7b3d47e3ad1e713
trieu.ngoquang <ngoquangtrieu99tb@gmail.com>
2020-06-01 21:37:11 +0700

    dump database 20200601

---
+++ database/20200601.sql
@@ -0,0 +1,216 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               8.0.19 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.3.0.5771
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for db_demo_t3h
+CREATE DATABASE IF NOT EXISTS `db_demo_t3h` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_demo_t3h`;
+
+-- Dumping structure for table db_demo_t3h.dbo_blog
+CREATE TABLE IF NOT EXISTS `dbo_blog` (
+  `blog_id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `image_blog` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`blog_id`),
+  KEY `fk_user_id` (`user_id`),
+  CONSTRAINT `fk_user_blog_id` FOREIGN KEY (`user_id`) REFERENCES `dbo_user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_cart
+CREATE TABLE IF NOT EXISTS `dbo_cart` (
+  `cart_id` int NOT NULL AUTO_INCREMENT,
+  `guid` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`cart_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_cart_product
+CREATE TABLE IF NOT EXISTS `dbo_cart_product` (
+  `cart_product_id` int NOT NULL AUTO_INCREMENT,
+  `cart_id` int DEFAULT NULL,
+  `product_entity_id` int DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  PRIMARY KEY (`cart_product_id`),
+  KEY `fk_cart_id` (`cart_id`),
+  KEY `fk_product_entity_id` (`product_entity_id`),
+  CONSTRAINT `fk_cart_id` FOREIGN KEY (`cart_id`) REFERENCES `dbo_cart` (`cart_id`),
+  CONSTRAINT `fk_product_entity_id` FOREIGN KEY (`product_entity_id`) REFERENCES `dbo_product_entity` (`product_entity_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_category
+CREATE TABLE IF NOT EXISTS `dbo_category` (
+  `category_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `short_desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_color
+CREATE TABLE IF NOT EXISTS `dbo_color` (
+  `color_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `short_desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`color_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_order
+CREATE TABLE IF NOT EXISTS `dbo_order` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `guid` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `customer_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone_number` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `ship` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_order_product
+CREATE TABLE IF NOT EXISTS `dbo_order_product` (
+  `order_product_id` int NOT NULL AUTO_INCREMENT,
+  `product_entity_id` int DEFAULT NULL,
+  `order_id` int DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`order_product_id`),
+  KEY `fk_order_id` (`order_id`),
+  KEY `fk_order_product_entity_id_idx` (`product_entity_id`),
+  CONSTRAINT `fk_order_id` FOREIGN KEY (`order_id`) REFERENCES `dbo_order` (`order_id`),
+  CONSTRAINT `fk_order_product_entity_id` FOREIGN KEY (`product_entity_id`) REFERENCES `dbo_product_entity` (`product_entity_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='		';
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_product
+CREATE TABLE IF NOT EXISTS `dbo_product` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `short_desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `main_image` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `brand` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`product_id`),
+  KEY `fk_category_id` (`category_id`),
+  CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `dbo_category` (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_product_entity
+CREATE TABLE IF NOT EXISTS `dbo_product_entity` (
+  `product_entity_id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int DEFAULT NULL,
+  `color_id` int DEFAULT NULL,
+  `size_id` int DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `amount` mediumtext COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`product_entity_id`),
+  KEY `fk_color_id` (`color_id`),
+  KEY `fk_product_id` (`product_id`),
+  KEY `fk_size_id` (`size_id`),
+  CONSTRAINT `fk_color_id` FOREIGN KEY (`color_id`) REFERENCES `dbo_color` (`color_id`),
+  CONSTRAINT `fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `dbo_product` (`product_id`),
+  CONSTRAINT `fk_size_id` FOREIGN KEY (`size_id`) REFERENCES `dbo_size` (`size_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_product_image
+CREATE TABLE IF NOT EXISTS `dbo_product_image` (
+  `product_image_id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int NOT NULL,
+  `link` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`product_image_id`),
+  KEY `fk_product_image_id` (`product_id`),
+  CONSTRAINT `fk_product_image_id` FOREIGN KEY (`product_id`) REFERENCES `dbo_product` (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_role
+CREATE TABLE IF NOT EXISTS `dbo_role` (
+  `role_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_size
+CREATE TABLE IF NOT EXISTS `dbo_size` (
+  `size_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `short_desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`size_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_user
+CREATE TABLE IF NOT EXISTS `dbo_user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gender` int DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_hash` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone_number` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table db_demo_t3h.dbo_user_role
+CREATE TABLE IF NOT EXISTS `dbo_user_role` (
+  `user_role_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  PRIMARY KEY (`user_role_id`),
+  KEY `fk_role_id` (`role_id`),
+  KEY `fk_user_id` (`user_id`),
+  CONSTRAINT `fk_role_id` FOREIGN KEY (`role_id`) REFERENCES `dbo_role` (`role_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `dbo_user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
