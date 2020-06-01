44d84ce4302070e8c5650cb381d1190feab9e360
trieu.ngoquang <ngoquangtrieu99tb@gmail.com>
2020-06-01 22:52:35 +0700

    add constants delivery status

---
+++ src/main/java/application/constant/DELIVERYSTATUS.java
@@ -0,0 +1,9 @@
+package application.constant;
+
+public class DELIVERYSTATUS {
+    private static final int CHO_THANH_TOAN = 1;
+    private static final int CHO_LAY_HANG = 2;
+    private static final int DANG_GIAO = 3;
+    private static final int DA_GIAO = 3;
+    private static final int DA_HUY = 3;
+}
