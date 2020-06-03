415045661c44faf589ca21a3fe51eb9f0fd00323
trieu.ngoquang <trieu.ngoquang@vti.com.vn>
2020-06-03 15:24:07 +0700

    create admin

--- src/main/java/application/data/repository/RoleRepository.java
+++ src/main/java/application/data/repository/RoleRepository.java
@@ -2,6 +2,14 @@ package application.data.repository;
 
 import application.data.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface RoleRepository extends JpaRepository<Role, Integer > {
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    @Query(value = "SELECT r.* FROM dbo_user_role ur " +
+            "INNER JOIN dbo_user u on (u.user_id = ur.user_id) " +
+            "INNER JOIN dbo_role r on (r.role_id = ur.role_id) " +
+            "WHERE ur.user_id = :id", nativeQuery = true)
+    Role getRoleByUser(@Param("id") Integer userId);
 }
